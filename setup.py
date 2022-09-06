14af6977b299abf22a0d637712d6754f3bab3b3e
Alexey Nesterov <vertigra.developer@gmail.com>
2022-09-06 12:31:17 +1000

    Init commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='FEETECH-Servo-Python-SDK',
+    version='0.0.1',
+    packages=['scservo_sdk'],
+    url='https://gitee.com/ftservo/SCServoSDK',
+    license='UNLICENSE',
+    author='vertigra',
+    author_email='a@nesterof.com',
+    description='This is source code from official feetech repository',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: The Unlicense (Unlicense)',
+        'Operating System :: POSIX :: Other',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Hardware :: Universal Serial Bus (USB) :: Communications Device Class (CDC)'
+    ]
+)
